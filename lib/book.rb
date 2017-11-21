cbb0ab8f5dc9b15da86970bb689e3f7af0155804
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-11-21 02:36:59 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,6 @@
 # my code below
+# worked clean and perfect when I simplified the code
+# and used attr_accessor and attr_reader instead of a bunch of def methods 
 
 class Book
   attr_accessor :author, :page_count, :genre
